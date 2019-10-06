09c290f5a327313ae5dc8ef2b6c1153eeab3a0ca
Isao Hara <haraisao@gmail.com>
2019-10-07 07:19:53 +0900

    update finddot.bat

--- find_dot.bat
+++ find_dot.bat
@@ -2,5 +2,12 @@
 where /Q /R "%ProgramFiles(x86)%" dot.exe
 if %errorlevel% == 0 (
     where /R "%ProgramFiles(x86)%" dot.exe
+    @exit /b 0
 )
-echo on
+where /Q /R %~d0\local dot.exe
+if %errorlevel% == 0 (
+    where /R %~d0\local dot.exe
+    @exit /b 0
+)
+@echo on
+@exit /b 1
